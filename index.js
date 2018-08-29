f111e4044d7e5cbd3e61bf22e9e95dc32f7bace7
Harry Bloch <harryfbloch@gmail.com>
2018-08-29 02:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ function setCart(c) {
   cart = c;
   return cart;
 }
-
+addToCart("pizza");
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * Math.floor(100));
+  var newItemObject = {"itemName" : `${item}`, "itemPrice" : price};
+  cart.push(newItemObject);
+  console.log(cart);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var message = "In your cart, you have";
+  if (cart.length !== 0) {
+    for (let i = 0; i < cart.length; i++){
+     message += ` ${cart[i].itemName}, at ${cart[i].price}`;
+    }
+    message += ".";
+  }else{
+    message = "Your shopping cart is empty."
+  }
+  return message;
 }
 
 function total() {
